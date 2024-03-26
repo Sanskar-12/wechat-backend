81d5fbbac1bce29399a74358be50cdebd63db73c
Sanskar-12 <sanskarv2004@gmail.com>
2024-03-26 08:43:38 +0530

    server continues

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ import { connectDB } from "./utils/features.js";
 import { config } from "dotenv";
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { NEW_MESSAGE } from "./constants/events.js";
+import { v4 as uuid } from "uuid";
 
 config({
   path: "./.env",
@@ -17,6 +21,9 @@ export const mode = process.env.NODE_ENV.trim() || "PRODUCTION";
 connectDB();
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {});
+export const userSocketIDs = new Map();
 
 //Using middlewares
 app.use(express.json());
@@ -30,8 +37,44 @@ app.get("/", (req, res) => {
   res.send("Server is Ready :)");
 });
 
+io.on("connection", (socket) => {
+  const sender = {
+    _id: "sdfsdfsdf",
+    name: "jsndjfnnsdf",
+  };
+
+  userSocketIDs.set(sender._id.toString(), socket.id);
+
+  console.log("a user connected", socket.id);
+
+  socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
+    const messageForRealTime = {
+      content: message,
+      _id: uuid(),
+      sender: {
+        _id: sender._id,
+        name: sender.name,
+      },
+      chat: chatId,
+      createdAt: new Date().toISOString(),
+    };
+
+    const messageForDB = {
+      content: message,
+      sender: sender._id,
+      chat: chatId,
+    };
+
+    console.log("new message", messageForRealTime);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
 app.use(errorMiddleware);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is Listening on port ${port} on ${mode} Mode`);
 });
